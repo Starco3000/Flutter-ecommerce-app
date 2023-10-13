3f7ef34ac186a58cef3ead80b6f84b3e715da46d
Nguyen Duc Minh <minhnguyen9686q6@gmail.com>
2023-10-13 12:58:16 +0700

    Hello screen, Login & SignUp page

---
+++ lib/consts/strings.dart
@@ -0,0 +1,26 @@
+const appName = "MT-Mart";
+const appVersion = "Version 1.0.0";
+const credits = "Copyright by Macus Nguyen & Huu Thang";
+const email = "Email";
+const emailHint = "abc@gmail.com";
+const password = "Password";
+const passHint = "Password";
+const retypePassword = "Retype Password";
+const name = "name";
+const nameHint = "johnson";
+const forgetPass = "Forget password?";
+const login = "Log in";
+const signup = "Sign Up";
+const anotherLogin = "Or";
+const createNewAccount = "Don't have an account yet?";
+const loginAccount = "Arealdy have an account? ";
+const privacyPolicy = "Privacy Policy";
+const termAndCond = "Terms and Conditions";
+
+const notitle = "";
+
+//home string
+const home = "Home",
+    categories = "Categories",
+    cart = "Cart",
+    account = "Account";
